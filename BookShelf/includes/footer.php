ef4df84c4bc1fa3b37b1c606edddd55071f0b180
Rohan.ch <153156315+Ron9248@users.noreply.github.com>
2024-06-22 13:53:19 +0545

    Add files via upload

---
+++ BookShelf/includes/footer.php
@@ -0,0 +1,11 @@
+<section class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy;BookShelf |<a href="https://www.facebook.com/profile.php?id=100033898460332" target="_blank">
+                    Designed by : Team BR</a>
+            </div>
+
+        </div>
+    </div>
+</section>
